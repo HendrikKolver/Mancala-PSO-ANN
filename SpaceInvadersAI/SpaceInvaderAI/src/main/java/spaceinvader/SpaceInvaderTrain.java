8ff9ded158adf54c4f8e1c7d07f5a4f693aa5373
Hendrik Kolver <hendrik.kolver@gmail.com>
2015-08-04 05:56:31 +0200

    training

--- SpaceInvadersAI/SpaceInvaderAI/src/main/java/spaceinvader/SpaceInvaderTrain.java
+++ SpaceInvadersAI/SpaceInvaderAI/src/main/java/spaceinvader/SpaceInvaderTrain.java
@@ -20,7 +20,7 @@ public class SpaceInvaderTrain {
     public static void main(String[] args) throws InterruptedException, FileNotFoundException, IOException { 
         Trainer trainer = new Trainer();
 //        NeuralNetwork nn = trainer.train(50, 3);
-        NeuralNetwork nn = trainer.trainWithOpponent(350, 4);
+        NeuralNetwork nn = trainer.trainWithOpponent(200, 5);
 
         System.out.println("Done");
         ThreadPool.executor.shutdown();
